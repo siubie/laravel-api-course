cc5ef55a1f02b5e53932e44955a537c1e873e729
Putra Prima Arhandi <putraprima@gmail.com>
2022-10-16 05:09:29 +0700

    Contoh Bad Practice

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/hello', function () {
+    // bad practice
+    return "hello world";
+});
