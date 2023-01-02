631229f9df1ee6cdcb0aef6b4b53e6e1fbe75d5b
Putra Prima Arhandi <putraprima@gmail.com>
2023-01-03 04:15:35 +0700

    fixing route api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Api\ApiAuthController;
 use App\Http\Controllers\FileUploadController;
-use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\Api\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
